fa3937a26cff6664f195bd887d806d6fa0a29e11
C.Charles <c.codoy.554412@umindanao.edu.ph>
2026-04-22 15:56:23 +0800

    i've added the action listener

--- GUI_rental.java
+++ GUI_rental.java
@@ -1,8 +1,14 @@
 package cCe103;
 
+import java.awt.event.ActionListener;
+import java.io.FileWriter;
 import javax.swing.*;
 
-public class GUI_rental extends JFrame{
+public class GUI_rental extends JFrame implements ActionListener{
+    JTextField cus;
+    JTextField cons;
+    JTextField bil;
+
     public static void main(String[]args){
         new GUI_rental();
     }
@@ -19,18 +25,19 @@ public class GUI_rental extends JFrame{
         add(bill).setBounds(50,120,150,25);
         
         //text field
-        JTextField cus = new JTextField();
+        cus = new JTextField();
         add(cus).setBounds(150,50,180,25); 
 
-        JTextField cons = new JTextField();
+        cons = new JTextField();
         add(cons).setBounds(150,85,180,25);
 
-        JTextField bil = new JTextField();
+        bil = new JTextField();
         add(bil).setBounds(150,120,180,25);
 
         //button
 
         JButton add = new JButton("add");
+        
         add(add).setBounds(50,150,90,25);
 
         JButton del = new JButton("Delete");
@@ -39,8 +46,12 @@ public class GUI_rental extends JFrame{
         JButton upd = new JButton("Update");
         add(upd).setBounds(240,150,90,25);
 
+        add.addActionListener(this);
+        del.addActionListener(this);
+        upd.addActionListener(this);
+
         //frame
-       setSize(400,235);
+        setSize(400,235);
 		setTitle("Codoy Rental");
 		setLayout(null);
 		setUndecorated(false);
@@ -48,6 +59,31 @@ public class GUI_rental extends JFrame{
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true); 
+    }
+    @Override
+    public void actionPerformed(java.awt.event.ActionEvent e) {
+        if(e.getSource() instanceof JButton){
+            JButton button = (JButton) e.getSource();
+            if(button.getText().equals("add")){
+                String type = cus.getText();
+                String consumption = cons.getText();
+                String billText = bil.getText();
+                try {
+                    savetoFile(type, consumption, billText);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                cus.setText("");
+                cons.setText("");
+                bil.setText("");
+            }
+        }
+    }
+
+    public static void savetoFile(String type, String consumption, String bill) throws Exception {
+        FileWriter fw = new FileWriter("RentalData.txt", true);
+        fw.write(type + "#" + consumption + "#" + bill + "\n");
+        fw.close();
     }
 
 }
